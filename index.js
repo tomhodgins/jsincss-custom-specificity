4e7c8bc64a1950c391c19f7fbbd5b463e57b99b2
Tommy Hodgins <tomhodgins@gmail.com>
2018-04-13 20:37:58 -0400

    Refactoring to use reduce()

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 export default (selector, number, rule) => {
 
-  let styles = ''
-  let count = 0
+  return Array.from(document.querySelectorAll(selector))
 
-  document.querySelectorAll(selector).forEach(tag => {
+    .reduce((styles, tag, count) => {
 
-    const attr = (selector+number).replace(/\W+/g, '')
-    const partial = `[data-specificity-${attr}="${count}"]`
-    const repeated = partial + partial.repeat(number)
+      const attr = (selector+number).replace(/\W+/g, '')
+      const partial = `[data-specificity-${attr}="${count}"]`
+      const repeated = partial + partial.repeat(number)
 
-    tag.setAttribute(`data-specificity-${attr}`, count)
-    styles += `${repeated} { ${rule} }\n`
-    count++
+      tag.setAttribute(`data-specificity-${attr}`, count)
+      styles += `${repeated} { ${rule} }\n`
+      count++
 
-  })
+      return styles
 
-  return styles
+    }, '')
 
 }
