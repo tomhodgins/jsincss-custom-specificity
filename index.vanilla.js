b6e06d010d1c23561b1439e215462aab17bb8a2b
Tommy Hodgins <tomhodgins@gmail.com>
2018-12-24 15:52:00 -0500

    Refactor to be cleaner/faster

--- index.vanilla.js
+++ index.vanilla.js
@@ -1,18 +1,14 @@
 export default (selector, number, rule) => {
-
-  return Array.from(document.querySelectorAll(selector))
-
-    .reduce((styles, tag, count) => {
-
-      const attr = (selector + number).replace(/\W/g, '')
-
+  const attr = (selector + number).replace(/\W/g, '')
+  const result = Array.from(document.querySelectorAll(selector))
+    .reduce((output, tag, count) => {
       const partial = `[data-specificity-${attr}="${count}"]`
       const repeated = partial + partial.repeat(number)
-
-      tag.setAttribute(`data-specificity-${attr}`, count)
-      styles += `${repeated} { ${rule} }\n`
-      return styles
-
-    }, '')
-
+      output.add.push({tag: tag, count: count})
+      output.styles.push(`${repeated} { ${rule} }`)
+      return output
+    }, {add: [], remove: [], styles: []})
+  result.add.forEach(tag => tag.tag.setAttribute(`data-specificity-${attr}`, tag.count))
+  result.remove.forEach(tag => tag.setAttribute(`data-specificity-${attr}`, ''))
+  return result.styles.join('\n')
 }
--- source/specificity.sexp
+++
@@ -1,8 +0,0 @@
-mixin('specificity', ['selector', 'number', 'rule'],
-  returnValue('Array.from(document.querySelectorAll(selector))',
-    reduceFunc(
-      createAttribute(['selector', 'number'],
-        prelude('      const partial = `[data-specificity-${attr}="${count}"]`\n\
-     const repeated = partial + partial.repeat(number)\n\n',
-          addAttribute('tag', 'specificity',
-            plainRule('${repeated}', '${rule}')))))))
